057a589c8b06ef71c8ec33a5469ca9245dd79fb7
restbackDev <jason.rbdev@gmail.com>
2025-04-04 18:39:17 -0700

    update movie file

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,1 +1,23 @@
-//"Test"
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+//const 
+
+
+// GET /users/:userId/movies/:moviesId "get all users"
+// TODO: add "verify token" once the models are all complete
+// TODO: query the movieID once the models are all complete
+router.get('/homepage/:movieId', async (req, res) => {
+    try {
+        // const currentMovie = await Movie.findById(:movieId);
+        // const res.json(currentMovie)
+        // request for all the reviews in this movie
+        
+        res.send("This this the current movie page ")
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
